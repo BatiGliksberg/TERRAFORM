47cdc64ea956315ad01e3739a1c415df4438f16f
B. Gliksberg <batyag@skyvar.co.il>
2024-01-25 10:03:31 +0200

    push

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,12 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name      = "NetworkWatcherRG"
-    storage_account_name     = "myfirsttrail"
-    container_name           = "terraform-demo"
-    key                      = "terraform.tfstate"
-  }
-  
-}
-
 provider "azurerm" {
+  subscription_id=var.subscription_id
   features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
     }
   }
-
-  subscription_id = var.subscription_id
 }
-
-# provider "azurerm" {
-#   subscription_id=var.subscription_id
-#   features {
-#     key_vault {
-#       purge_soft_delete_on_destroy    = true
-#       recover_soft_deleted_key_vaults = true
-#     }
-#   }
-# }
 
 resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
